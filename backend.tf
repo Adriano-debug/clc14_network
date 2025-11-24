122fb5ef42b58358d741b980d579b3bbad4edb36
Vinicius Cavalcanti <127455645+uid1root@users.noreply.github.com>
2025-11-23 23:47:42 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-vini-remote"
+    bucket       = "terraform-vini-clc14-remote"
     key          = "network/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
